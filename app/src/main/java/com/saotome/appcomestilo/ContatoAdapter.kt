5ba306f7e7b3a6aedeadc0146d45b7bac1270577
Saotomefirst <jorge.appleseed@gmail.com>
2021-12-05 22:28:57 -0300

    Colocando o RecyclerView na tela do app
    Integramos o Adapter para o Contato na tela principal do app.
    
    Corrigido bug no card do contato que fazia cada item ter o tamanho da tela.
    
    Adicionado layout de card, espaçamento e cantos arredondados ao visual do card.
    
    Criados métodos de acesso e gerenciamento do RecyclerView na MainActivity.kt

--- app/src/main/java/com/saotome/appcomestilo/ContatoAdapter.kt
+++ app/src/main/java/com/saotome/appcomestilo/ContatoAdapter.kt
@@ -3,6 +3,8 @@ package com.saotome.appcomestilo
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContatoAdapter : RecyclerView.Adapter <ContatoAdapter.ContatoAdapterViewHolder> (){
@@ -24,14 +26,25 @@ class ContatoAdapter : RecyclerView.Adapter <ContatoAdapter.ContatoAdapterViewHo
         return lista.size
     }
 
+    //Método público que acessa o Adapter
+    fun updateList(listaAtualizada: List<Contato>) {
+        lista.clear()
+        lista.addAll(listaAtualizada)
+        notifyDataSetChanged()
+    }
 
     // Classe que faz o gerenciamento individual dos itens
     class ContatoAdapterViewHolder (itemView: View) : RecyclerView.ViewHolder (itemView) {
 
         // Aqui temos a declaração de cada elemento a ser montado dentro de um Card
+        private val tvNome: TextView = itemView.findViewById(R.id.tv_nome)
+        private val tvTelefone: TextView = itemView.findViewById(R.id.tv_telefone)
+        private val ivFoto: ImageView = itemView.findViewById(R.id.iv_foto)
 
         fun bind (contato: Contato) {
-
+            tvNome.text = contato.nome
+            tvTelefone.text = contato.telefone
+            //TODO("Atualizar a Fotografia")
         }
     }
 
--- app/src/main/java/com/saotome/appcomestilo/MainActivity.kt
+++ app/src/main/java/com/saotome/appcomestilo/MainActivity.kt
@@ -2,10 +2,54 @@ package com.saotome.appcomestilo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    //Obtemos o componente RecyclerView do XML
+    private val rvLista: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_lista)
+    }
+
+    private val adapter = ContatoAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindView()
+        updateList()
+    }
+
+    fun bindView() {
+        // Definimos o adapter do componente da tela como sendo o que criamos
+        rvLista.adapter = adapter
+
+        // Definimos o layout manager
+        rvLista.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList () {
+        // Criamos uma lista mock apenas para vermos dados na tela
+        adapter.updateList(
+            arrayListOf<Contato>(
+                Contato(
+                    nome = "Jorge Freitas",
+                    telefone = "(21)987-654-321",
+                    foto = "foto.png"
+                ),
+                Contato(
+                    nome = "Thaís Gallo",
+                    telefone = "(21)123-456-789",
+                    foto = "foto2.png"
+                ),
+                Contato(
+                    nome = "Marilena Lopes",
+                    telefone = "(21)123-654-987",
+                    foto = "foto3.png"
+                ),
+            )
+        )
     }
 }
