a708fe902f89796f853657318d33ddc6db0c4809
Saotomefirst <jorge.appleseed@gmail.com>
2021-12-18 19:27:33 -0300

    TDD - Preparando Classe de Testes
    Criado um método de teste para servir de base para a criação de um novo arquivo, a ser criado posteriormente

---
+++ app/src/test/java/com/saotome/appcomestilo/ContatoHelperTest.kt
@@ -0,0 +1,21 @@
+package com.saotome.appcomestilo
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ContatoHelperTest {
+    @Test
+    fun `Quando chamar o metodo getListaContatos() com 2 Contatos, deve retornar uma lista de 2 contatos`() {
+        // Sessão de Preparação
+        TODO ("Implementar a prepação do mock")
+        // Sessão de Validação
+        TODO ("Validar valores de retorno do método")
+        //assertEquals(4, 2 + 2)
+    }
+}
