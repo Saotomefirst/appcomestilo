6b6a1cc9b5528fdb1b594578cdd2052dc02fc345
Saotomefirst <jorge.appleseed@gmail.com>
2021-12-19 00:48:26 -0300

    Atualizando para ViewBinding
    Trocadas chamadas de variáveis para usar o ViewBinding.

--- app/src/main/java/com/saotome/appcomestilo/MainActivity.kt
+++ app/src/main/java/com/saotome/appcomestilo/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.saotome.appcomestilo
 
-import android.content.Context
-import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
@@ -9,30 +7,41 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
-import androidx.core.content.edit
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import com.saotome.appcomestilo.ContatoDetalhe.Companion.CONTATO_EXTRA
+import com.saotome.appcomestilo.databinding.ActivityMainBinding
+import com.saotome.appcomestilo.databinding.DrawerMenuBinding
 
 class MainActivity : AppCompatActivity(), ClickItemContatoListener {
 
     //Obtemos o componente RecyclerView do XML
     private val rvLista: RecyclerView by lazy {
-        findViewById<RecyclerView>(R.id.rv_lista)
+        //findViewById<RecyclerView>(R.id.rv_lista)
+        activityMainBinding.rvLista
     }
 
     private val adapter = ContatoAdapter(this)
+    private lateinit var drawerMenuBinding: DrawerMenuBinding
+    private lateinit var activityMainBinding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.drawer_menu)
+
+        drawerMenuBinding = DrawerMenuBinding.inflate(layoutInflater)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        val drawerMenuView = drawerMenuBinding.root
+        setContentView(drawerMenuView)
+
+        val activityMainView = activityMainBinding.root
+        setContentView(activityMainView)
+
+        //setContentView(R.layout.drawer_menu)
 
         inicializarDrawer()
         fetchListaContatos()
@@ -87,10 +96,12 @@ class MainActivity : AppCompatActivity(), ClickItemContatoListener {
 
     //Inicialização manual do Drawer Layout
     private fun inicializarDrawer () {
-        //TODO atualizar parâmetros para ViewBindindg!
-        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val drawerLayout = drawerMenuBinding.drawerLayout
+        //val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
         //val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        val toolbar = activityMainBinding.toolbar
+        //val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
